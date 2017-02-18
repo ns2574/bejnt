08ee667469c20f6f19697cd860a1c1989919590b
Benjamin Jonke <bjonke00@gmail.com>
2017-02-18 17:26:43 -0500

    added dialog buttons

--- BuddySystem2/app/src/main/java/com/example/nielasultana/buddysystem/MainActivity.java
+++ BuddySystem2/app/src/main/java/com/example/nielasultana/buddysystem/MainActivity.java
@@ -1,13 +1,40 @@
 package com.example.nielasultana.buddysystem;
-
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void openDialog(View view) {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        // Add the buttons
+        builder.setPositiveButton("Non-Severe", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                // User clicked OK button
+            }
+        });
+        builder.setNeutralButton("Severe", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                // User cancelled the dialog
+            }
+        });
+
+        // Create the AlertDialog
+        AlertDialog dialog = builder.create();
+
+        dialog.show();
+
+    }
+
 }

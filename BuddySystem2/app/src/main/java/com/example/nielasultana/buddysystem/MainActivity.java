55820c7940f93c67f288ff528aa20eca528ac34d
Benjamin Jonke <bjonke00@gmail.com>
2017-02-18 18:11:47 -0500

    added listView

--- BuddySystem2/app/src/main/java/com/example/nielasultana/buddysystem/MainActivity.java
+++ BuddySystem2/app/src/main/java/com/example/nielasultana/buddysystem/MainActivity.java
@@ -1,18 +1,28 @@
 package com.example.nielasultana.buddysystem;
+import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 
-public class MainActivity extends AppCompatActivity{
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String[] peopleInNeed = {"1", "2", "3", "4"};
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.list_layout, peopleInNeed);
+        ListView helpyList = (ListView) findViewById(R.id.helpyList);
+        helpyList.setAdapter(adapter);
     }
+
 
     public void openDialog(View view) {
 
@@ -36,5 +46,6 @@ public class MainActivity extends AppCompatActivity{
         dialog.show();
 
     }
+
 
 }
